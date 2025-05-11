4d16f440ebcfed5636c9bea0ab27e7526a10f646
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-05-11 15:54:34 +0800

    made get_word into a module

--- dev/get_word_dict.py
+++ dev/get_word_dict.py
@@ -1,10 +1,32 @@
 import json
 
-with open("dict/jmdict-eng-3.6.1.json", encoding="utf-8") as file:
-    data = json.load(file)
+def json2_print(data: dict):
 
-print(data["version"])
-print(data["words"][0].keys())
-print(data["words"][30]["kanji"])
-print(data["words"][30]["kana"])
-print(data["words"][30]["sense"])
+    print(json.dumps(data, indent=4,  ensure_ascii=False))
+
+if __name__ == "__main__":
+
+    with open("dict/jmdict-eng-3.6.1.json", encoding="utf-8") as file:
+        data = json.load(file)
+
+    print(data["version"])
+
+    words = data["words"]
+
+    print(words[0].keys())
+    print(type(words[30]))
+    print(words[30]["kanji"])
+
+    json2_print(words[30])
+
+    print(type(words[30]["kanji"]))
+    print(type(words[30]["kana"]))
+    print(type(words[30]["sense"]))
+    print(len(words[30]["kanji"]))
+
+    print(words[100]["kanji"])
+    print(words[100]["kana"])
+    print(words[100]["sense"][0])
+    json2_print(words[100])
+
+    json2_print(words[150])
---
+++ tools/README.md
@@ -0,0 +1,3 @@
+## extract_words.py
+- Sometimes the verb, tense etc. of a word is seperated.
+- TODO: hold the word with the next word and compare if it exists in the dict, if not discard (this could be computationally expensive)
